bf1d0c606a2b2b591f281dd06f6efb314501c5e3
Eaint Eaint <eaintmohmohtun46578@gmail.com>
2023-07-11 14:12:11 +0900

    third commit

--- question.js
+++ question.js
@@ -1,15 +1,15 @@
- const toggleContents = (event) => {
-    const clickedQuestion = event.currentTarget;
-    const content = clickedQuestion.querySelector('.content');
+const toggleContents = (event) => {
+  const clickedQuestion = event.currentTarget;
+  const content = clickedQuestion.querySelector('.content');
 
-    if (content.style.display === 'none') {
-      content.style.display = 'block';
-    } else {
-      content.style.display = 'none';
-    }
-  };
+  if (content.style.display === 'none') {
+    content.style.display = 'block';
+  } else {
+    content.style.display = 'none';
+  }
+};
 
-  const questions = document.querySelectorAll('.question');
-  questions.forEach((question) => {
-    question.addEventListener('click', toggleContents);
-  });
+const questions = document.querySelectorAll('.question');
+questions.forEach((question) => {
+  question.addEventListener('click', toggleContents);
+});
